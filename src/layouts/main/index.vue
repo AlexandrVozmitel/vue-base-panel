a214c69e87f1e26cfe8cfc6cce3bdd91cfa650b5
Alexandr Vozmitel <avozmitel@mail.ru>
2020-08-31 23:57:58 +0300

    рефактор навигационного бара

--- src/layouts/main/index.vue
+++ src/layouts/main/index.vue
@@ -1,21 +1,15 @@
 <template>
 	<div>
 		<b-navbar toggleable="lg" type="dark" variant="dark">
-			<b-navbar-brand href="#">LOGO</b-navbar-brand>
+			<b-navbar-brand :to="{name: 'home'}">Service Panel</b-navbar-brand>
 			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 			<b-collapse id="nav-collapse" is-nav>
-			<b-navbar-nav>
-					<b-nav-item>
-						Страничка
+				<b-navbar-nav>
+					<b-nav-item v-for="(item, index) in menu" :key="index" active-class="active" :to="item.route">
+						{{item.name}}
 					</b-nav-item>
-					<b-nav-item >
-						Страничка
-				</b-nav-item>
-					<b-nav-item>
-						Страничка
-				</b-nav-item>
 				</b-navbar-nav>
-					<b-navbar-nav class="ml-auto">
+				<b-navbar-nav class="ml-auto">
 					<b-nav-item-dropdown right text="User" >
 						<b-dropdown-item href="#">Profile</b-dropdown-item>
 						<b-dropdown-item :to="{name:'signIn'}">Sign Out</b-dropdown-item>
@@ -23,10 +17,29 @@
 				</b-navbar-nav>
 			</b-collapse>
 		</b-navbar>
-		<b-container fluid="fluid">
+		<b-container fluid="fluid" class="pt-2">
 			<slot />
 		</b-container>
 	</div>
 </template>
-<style>
-</style>
+
+<script>
+	export default {
+		data: () => ({
+			menu: [
+				{
+					name: 'Группы хостов',
+					route: {
+						name: 'hostGroup'
+					},
+				},
+				{
+					name: 'Пользователи',
+					route: {
+						name: 'userList'
+					},
+				}
+			]
+		}),
+	}
+</script>
