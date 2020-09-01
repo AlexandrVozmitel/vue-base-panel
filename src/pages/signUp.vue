9c29a5e5fa7d79cb798a6cdf85776e9748059be1
obet-hg <yuzhakov975@gmail.com>
2020-09-01 20:39:10 +0500

    регистрация

--- src/pages/signUp.vue
+++ src/pages/signUp.vue
@@ -1,34 +1,59 @@
 <template>
-	<div class="box ml-auto mr-auto">
-		<form>
-			<h3>Sign Up</h3>
+    <div class="box ml-auto mr-auto">
+        <form>
+            <h3>Sign Up</h3>
 
-			<b-form-group
-					label="Full name"
-			>
-				<b-form-input></b-form-input>
-			</b-form-group>
+            <b-form-group
+                    label="Full name"
+            >
+                <b-form-input v-model="name"></b-form-input>
+            </b-form-group>
+            <b-form-group
+                    label="Email"
+            >
+                <b-form-input v-model="email"></b-form-input>
+            </b-form-group>
+            <b-form-group
+                    label="Login"
+            >
+                <b-form-input v-model="login"></b-form-input>
+            </b-form-group>
 
-			<b-form-group
-					label="Login"
-			>
-				<b-form-input></b-form-input>
-			</b-form-group>
+            <b-form-group
+                    label="Password"
+            >
+                <b-form-input v-model="password"></b-form-input>
+            </b-form-group>
 
-			<b-form-group
-					label="Password"
-			>
-				<b-form-input></b-form-input>
-			</b-form-group>
+            <b-button block variant="dark" v-on:click="register" :to="{name: 'home'}">Sign Up</b-button>
 
-			<b-button block type="submit" variant="dark">Sign Up</b-button>
-
-			<p class="forgot-password text-right">
-				Already registered
-				<router-link to="/sign-in">sign in?</router-link>
-			</p>
-		</form>
-	</div>
+            <p class="forgot-password text-right">
+                Already registered
+                <router-link to="/sign-in">sign in?</router-link>
+            </p>
+        </form>
+    </div>
 </template>
-<style>
-</style>
+<script>
+	export default {
+		data: () => ({
+			login:'',
+			name:'',
+			email:'',
+			password:'',
+		}),
+		methods: {
+			async register() {
+				await this.axios.post('http://api.dev.cmtyomg.com/cto1/user',{
+					params:{
+						login: this.login,
+						name: this.name,
+						email: this.email,
+						password: this.password,
+					}}
+				)
+			},
+		},
+
+	};
+</script>
